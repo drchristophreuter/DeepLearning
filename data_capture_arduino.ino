85874eb2fafb023b1a7818c3bb65e65e08841bc9
Cris <148689493+drchristophreuter@users.noreply.github.com>
2023-10-28 18:47:50 +0200

    Update data_capture_arduino.ino

--- data_capture_arduino.ino
+++ data_capture_arduino.ino
@@ -1,1 +1,4 @@
+// Dr. Christoph Reuter 28.10.2023
+// Die Datenerfassung erfolgt über einen Arduino Mega mit Adafruit AS7262 6-Channel Visible Light / Color Sensor Breakout
+// Die Sensordaten werden über die serielle Schnittstelle an den Linuxserver Übertragen und dort weiterverarbeitet
 
